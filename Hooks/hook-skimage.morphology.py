8fbaa4a9ec0e7d384e97cbc4a56f12eb1274cd34
Jacob Bumgarner <70919881+JacobBumgarner@users.noreply.github.com>
2022-01-10 00:10:37 -0500

    Delete Hooks directory

--- Hooks/hook-skimage.morphology.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-hiddenimports = ['skimage.filters.rank.core_cy_3d']
--- Hooks/hook-vtk.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-hiddenimports = ['vtkmodules','vtkmodules.all','vtkmodules.qt.QVTKRenderWindowInteractor',
-                 'vtkmodules.util','vtkmodules.util.numpy_support',
-                 'vtkmodules.vtkFiltersGeneral']
