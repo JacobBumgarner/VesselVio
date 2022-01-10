0bb31bd5e635c08dfd5da2dbfc51a4a145154d4f
Jacob Bumgarner <70919881+JacobBumgarner@users.noreply.github.com>
2022-01-10 00:03:59 -0500

    Update VesselVio.py

--- VesselVio.py
+++ VesselVio.py
@@ -1,116 +1,104 @@
-
 """
-By executing this program in a terminal, the VesselVio app will be built and can be used to analyze and visualize files.
-
-@author: jacobbumgarner
+This is the GUI entry point for VesselVio. By executing VesselVio.py, the app will be built and can be used locally.
 """
 
-import sys, platform
-from os import path, environ, getcwd, mkdir
+__author__    = 'Jacob Bumgarner <jrbumgarner@mix.wvu.edu>'
+__license__   = 'GPLv3 - GNU General Pulic License v3 (see LICENSE)'
+__copyright__ = 'Copyright © 2021 by Jacob Bumgarner'
+__webpage__   = 'https://jacobbumgarner.github.io/VesselVio_Web/'
+__download__  = 'https://jacobbumgarner.github.io/VesselVio_Web/Downloads'
 
-# At the moment I import everything from the jump. I was experiencing issues with pyinstaller when some of these were excluded from the main file.
-from PyQt5.QtGui import QFont, QPalette
-from PyQt5.QtCore import Qt, QSize, pyqtSlot
-from PyQt5.QtWidgets import (QApplication,
-                            QWidget, QLabel, QSpacerItem, QPushButton, QMessageBox, QScrollArea, QButtonGroup,
-                            QCheckBox, QDoubleSpinBox, QGroupBox, QFrame, QComboBox, QSpinBox, QRadioButton,
-                            QListWidget, QListWidgetItem, QStackedWidget, QSlider,
-                            QInputDialog, QLineEdit, QFileDialog,
-                            QDialog, QColorDialog, QSplitter,
-                            QGridLayout, QHBoxLayout, QVBoxLayout,
-                            QMainWindow, QProgressBar)
 
-import numpy as np
-import matplotlib as mpl
-import vtk
+
+import sys
+
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import (QApplication, QStackedWidget,
+                            QMainWindow, QStatusBar, QMessageBox)
+from multiprocessing import freeze_support
+
+from pyvistaqt import QtInteractor
 import pyvista as pv
-from pyvistaqt import QtInteractor, BackgroundPlotter
 
-from Library import Backend_Processing as bep
-from Library.helpers import remove_legend, prep_scalars_update, get_rgb, eliminate_spacing, get_dir, get_cwd, load_results_dir
-
-# Import our UIs
+# Import UIs
+from Library.UI import QtObjects as QtO
 from Library.UI import LeftMenu as lm
-from Library.UI import Page1 as p1
-from Library.UI import Page2 as p2
+from Library.UI import Analysis_Page as p1
+from Library.UI import Visualization_Page as p2
+from Library.UI import Annotation_Page as p3
+from Library.UI import UpdateAlert
+from Library import QtThreading as QtTh
+from Library import Image_Processing as ImProc
 
 
-# Get our working directory
-wd = get_cwd()
+######
+version = "V 1.1.0"
+######
+     
 
-# Build site for our application.
+# Main window for the application.
 class mainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
-
-        ## Setup the main window.        
+        super().__init__()        
+        ## Setup the main window.
+        self.statusBar = QStatusBar()
+        self.setStatusBar(self.statusBar)
+        self.statusBar.setVisible(False)
+                
+        self.plotter = QtInteractor()
+        light = pv.Light(light_type='headlight', intensity=0.1)
+        self.plotter.add_light(light)
+        
         # Name & Color.
         self.setWindowTitle('VesselVio')
-        self.centralWidget = QWidget()
+        self.centralWidget = QtO.new_widget()
         self.setCentralWidget(self.centralWidget)        
-
-        # Size and layout
-        self.resize(750, 500)
-        self.setMinimumWidth(750)
-        self.maingrid = QHBoxLayout()
-        self.maingrid.setSpacing(0)
-        self.maingrid.setContentsMargins(0,0,0,0)
-        self.centralWidget.setLayout(self.maingrid)
+        self.centralLayout = QtO.new_layout(self.centralWidget, no_spacing=True)
         
         # Setup the individual pages of the application.
         self.qStack = QStackedWidget()
 
-        left_menu = lm.LeftMenu()
-        self.page1 = p1.Page1()
-        self.page2 = p2.Page2()
+        self.leftMenu = lm.LeftMenu(version)
         
-        self.maingrid.addWidget(left_menu)
-        self.qStack.addWidget(self.page1)
-        self.qStack.addWidget(self.page2)
+        self.page1 = p1.AnalysisPage()
+        self.page2 = p2.VisualizationPage(self.statusBar, self.plotter, self)
+        self.page3 = p3.AnnotationPage()
         
-        ## TODO
-        page3 = self.initPage3()
-        self.maingrid.addWidget(self.qStack)
+        self.pageStack = QtO.new_stacked([self.page1,
+                                          self.page2, self.page3])
         
-        # Connect our left menu to our pages.
-        left_menu.qList.currentRowChanged.connect(
-            self.qStack.setCurrentIndex)
-        left_menu.qList.currentRowChanged.connect(self.update_dirs)
-        left_menu.qList.setCurrentRow(0)        
+        # Connect sour left menu to our pages.
+        self.leftMenu.pageTabs.currentRowChanged.connect(
+            self.pageStack.setCurrentIndex)
+        self.leftMenu.pageTabs.setCurrentRow(0)        
         
+        # Add pages to the app
+        QtO.add_widgets(self.centralLayout, [self.leftMenu, self.pageStack])
         
-        # Run our tiny file to prep our numba jitc.
-        bep.process_file('./Library/tiny.nii')
+        # Make sure we delete the labeled_cache_volume if it exists
+        ImProc.clear_labeled_cache()
         
-        # Present application
-        self.show()
-      
-    def update_dirs(self):
-        results_dir = load_results_dir()
-        self.page1.results_path_location.setText(results_dir)
+        # Run our tiny file to prep our numba jit compilers.
+        QtTh.prepare_compilers()
         
-        
-    # Page 3 Construction
-    def initPage3(self):
-        page3 = QWidget()
-        page3layout = QHBoxLayout(page3)
-        page3layout.setSpacing(0)
-        page3layout.setContentsMargins(20,0,0,0)
-        label = QLabel("Coming in future update.")
-        page3layout.addWidget(label, alignment=Qt.AlignCenter)
-        self.qStack.addWidget(page3)
-     
-    # Close all associated widgets if the main thread is ended.
+        # Check for updates
+        UpdateAlert.local_version = version[2:]
+        QTimer.singleShot(500, UpdateAlert.version_check)
+        return
+    
+    # Close all associated widgets if the main thread is ended.s
     def closeEvent(self, event):
-        self.page1.a_thread.stop()
         try:
+            self.page1.a_thread.stop()
             self.page2.v_thread.close()
         except:
             pass
         event.accept()
 
-
+     
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = mainWindow()
-    sys.exit(app.exec_())
+    freeze_support()
+    app = QApplication(sys.argv)  
+    main_app = mainWindow()
+    main_app.show()
+    sys.exit(app.exec_()) 
