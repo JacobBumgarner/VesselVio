db4f9abfd5d8012340d350020bde5baa7d52c00f
Jacob Bumgarner <70919881+JacobBumgarner@users.noreply.github.com>
2022-01-15 12:52:48 -0500

    Update Visualization_Page.py

--- Library/UI/Visualization_Page.py
+++ Library/UI/Visualization_Page.py
@@ -66,7 +66,7 @@ class VisualizationPage(QSplitter):
         
         # self.splitterMoved.connect(self.redraw_widgets)
         ## Default loading file
-        default_file = os.path.join(helpers.get_cwd(), 'Library', 'Volumes', 'Cube.nii')
+        default_file = os.path.join(helpers.get_cwd(), 'Library', 'Volumes', 'Demo Volume.nii')
         default_file = helpers.std_path(default_file)
         self.files.file1 = str(default_file)
         ## Page layout setup
